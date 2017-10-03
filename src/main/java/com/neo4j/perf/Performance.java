fc7bcb6c43cb4b93eac924c6e7a30cc0538feff6
GU372940 <gurpreet.singh44@wipro.com>
2017-10-03 18:02:52 +0530

    neo4-examle

--- src/main/java/com/neo4j/perf/Performance.java
+++ src/main/java/com/neo4j/perf/Performance.java
@@ -1,12 +1,4 @@
-/*******************************************************************************
- * Copyright (c) 2017  Wipro Digital. All rights reserved.
- * 
- *  Contributors:
- *      Wipro Digital - Looking Glass Team.
- *      
- *      
- *      May 5, 2017
- ******************************************************************************/
+
 package com.neo4j.perf;
 
 import org.perf4j.StopWatch;
